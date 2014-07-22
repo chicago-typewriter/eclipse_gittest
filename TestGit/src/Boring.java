d4fd634dc8b3e332738e8224aa529e9befcbf97d
chicago-typewriter <myEmail@email.com.br>
2014-07-22 20:14:40 -0300

    A simples teste of branch to eclipse

---
+++ TestGit/src/Boring.java
@@ -0,0 +1,17 @@
+
+public class Boring {
+	private String boring;
+
+	public String getBoring() {
+		return boring;
+	}
+
+	public void setBoring(String boring) {
+		this.boring = boring;
+	}
+
+	@Override
+	public String toString() {
+		return "Boring [boring=" + boring + "]";
+	}
+}
---
+++ TestGit/src/MyMain.java
@@ -0,0 +1,10 @@
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		Boring boring = new Boring();
+		boring.setBoring("So boring");
+		System.err.println(boring);
+	}
+
+}
